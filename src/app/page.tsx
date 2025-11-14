85a8713abf140032fd774473b268298a11d213fb
Sean Marinas <seanmarinas@gmail.com>
2025-11-14 16:48:32 +0800

    feat: added about page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import LandingAbout from "@/components/landing/landing-about";
 import LandingHero from "@/components/landing/landing-hero";
 
 const Home = () => {
   return (
     <div className="">
       <LandingHero />
+      <LandingAbout />
     </div>
   );
 };
---
+++ src/components/landing/landing-about.tsx
@@ -0,0 +1,26 @@
+const LandingAbout = () => {
+  return (
+    <div className="px-5 py-10">
+      <div className="max-w-7xl mx-auto">
+        <h2 className="text-site-muted mb-8 font-light md:text-2xl">
+          01. About
+        </h2>
+
+        <p className="text-2xl md:text-4xl font-light">
+          In the bustling streets of{" "}
+          <span className="text-site-muted">Tanjong Pagar</span>, Signal
+          whispers the secrets of{" "}
+          <span className="text-site-muted">
+            Seou&apos;s after-dark culture.
+          </span>{" "}
+          Born from a Seoul native's passion for authentic Korean hospitality,
+          this intimate sanctuary transforms every evening into a celebration of{" "}
+          <span className="text-site-muted">genuine connection</span> and{" "}
+          <span className="text-site-muted">timeless tradition.</span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default LandingAbout;
